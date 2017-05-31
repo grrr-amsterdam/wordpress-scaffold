a6371af2bc24e6879a1f67e680a5e62dfb94914c
Ramiro Hammen <ramiro@grrr.nl>
2017-05-31 18:10:17 +0200

    added post create project script

---
+++ lib/Util/StringUtil.php
@@ -0,0 +1,24 @@
+<?php 
+namespace Grrr\Warp\Util;
+
+class StringUtil {
+    /**
+     * Interpolate strings with variables
+     *
+     * @param string $str The string
+     * @param array $vars The variables
+     * @return string The interpolated string
+     */
+    static public function interpolate($str, array $vars) {
+        $keys = array_keys($vars);
+        $vals = array_values($vars);
+        // surround keys by "%"
+        array_walk(
+            $keys, function (&$s) {
+                $s = '%' . $s . '%';
+            }
+        );
+        $str = str_replace($keys, $vals, $str);
+        return $str;
+    }
+}
