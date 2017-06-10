0b8ba2ac287e5779bfb0aa6010b159d46ba6abd7
Ramiro Hammen <ramiro@grrr.nl>
2017-06-10 20:46:58 +0200

    Fixed php syntax error
    Removed visibility modifier for class constant, because that is only possible since
    PHP 7.1.0

--- web/app/themes/wordpress-scaffold/lib/Grrr/Acf/Setup.php
+++ web/app/themes/wordpress-scaffold/lib/Grrr/Acf/Setup.php
@@ -4,7 +4,7 @@ namespace Grrr\Acf;
 
 class Setup {
 
-    private const ACF_FIELDS_VERSION = '0.0.0';
+    const ACF_FIELDS_VERSION = '0.0.0';
 
     public function __construct() {
         if (!defined('ACF_FIELDS_VERSION')) {
