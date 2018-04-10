ffb42ed10c597f716f3080a3c0f6868463900f6b
Koen Schaft <koen@grrr.nl>
2018-04-10 09:52:30 +0200

    Add some extra defaults
    - Allow svg uploads
    - Clear W3TC after ACF options save
    - Fix CloudFront stripping the UA header
    - Add admin footer timing comment

--- web/app/themes/wordpress-scaffold/lib/Sage/Extras.php
+++ web/app/themes/wordpress-scaffold/lib/Sage/Extras.php
@@ -68,3 +68,53 @@ function yoasttobottom() {
     return 'low';
 }
 add_filter('wpseo_metabox_prio', __NAMESPACE__ . '\\yoasttobottom');
+
+/**
+ * Allow svg uploads
+ */
+function mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', __NAMESPACE__ . '\\mime_types');
+
+/**
+ * Clear W3 Total Cache on saving ACF Options
+ * Executes AFTER save_post hook (else use 1 as priority).
+ */
+function custom_cache_clearing($post_id) {
+    // just execute if the $post_id has either of these Values. Skip on Autosave
+    if (!defined('DOING_AUTOSAVE') && class_exists('W3_Plugin_TotalCacheAdmin')) {
+        $plugin_totalcacheadmin = & w3_instance('W3_Plugin_TotalCacheAdmin');
+        $plugin_totalcacheadmin->flush_all();
+    }
+    return $post_id;
+}
+add_action('acf/save_post', __NAMESPACE__ . '\\custom_cache_clearing', 20);
+
+/**
+* Overrides the function user_can_richedit and only checks the
+* user preferences instead of doing UA sniffing.
+* @see: https://benjaminhorn.io/code/wordpress-visual-editor-not-visible-because-of-user-agent-sniffing/
+*/
+function user_can_richedit_custom() {
+    global $wp_rich_edit;
+    if (get_user_option('rich_editing') == 'true' || !is_user_logged_in()) {
+        $wp_rich_edit = true;
+        return true;
+    }
+    $wp_rich_edit = false;
+    return false;
+}
+
+add_filter('user_can_richedit', __NAMESPACE__ . '\\user_can_richedit_custom');
+
+/**
+ * Admin footer timing
+ */
+function my_admin_footer_function() {
+    ?>
+    <!--<?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?> seconds.-->
+    <?php
+}
+add_action('admin_footer-index.php', __NAMESPACE__ . '\\my_admin_footer_function');
