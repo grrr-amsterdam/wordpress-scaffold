03884272df71ad4b7d1e75aeceaabbe1307817af
Koen Schaft <koen@grrr.nl>
2018-04-11 17:52:17 +0200

    Add default styling for browser-warning

--- web/app/themes/wordpress-scaffold/base.php
+++ web/app/themes/wordpress-scaffold/base.php
@@ -36,7 +36,7 @@ use Roots\Sage\Wrapper;
 
         <!--[if IE]>
         <div class="browser-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+            <?php _e('You are using a <strong>strongly outdated</strong> browser. Please <a href="https://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'sage'); ?>
         </div>
         <![endif]-->
     </body>
