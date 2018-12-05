5167b33de0d357857bbb39125865f4d3f64fd34b
Koen Schaft <koen@grrr.nl>
2018-12-05 16:08:04 +0100

    Update browser-warning link due to TLS 1.2 usage

--- web/app/themes/wordpress-scaffold/base.php
+++ web/app/themes/wordpress-scaffold/base.php
@@ -36,7 +36,7 @@ use Roots\Sage\Wrapper;
 
         <!--[if IE]>
         <div class="browser-warning">
-            <?php _e('You are using a <strong>strongly outdated</strong> browser. Please <a href="https://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+            <?php _e('You are using a <strong>strongly outdated</strong> browser. Please <a href="http://outdatedbrowser.com/" target="_blank">upgrade your browser</a> to improve your experience.', 'sage'); ?>
         </div>
         <![endif]-->
     </body>
