b13ef00739a449d3c9fcb85f91de4ace74c7abbc
Koen Schaft <koen@grrr.nl>
2019-07-03 17:00:37 +0200

    Disable fatal error handler for all environments

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,4 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -3,5 +3,6 @@
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
