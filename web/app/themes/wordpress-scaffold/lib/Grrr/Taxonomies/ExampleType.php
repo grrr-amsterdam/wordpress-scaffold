ae274bec1220478e1a6b392b4b720f7431bb1f3d
Koen Schaft <koen@grrr.nl>
2019-07-31 16:16:22 +0200

    Add example page template

--- web/app/themes/wordpress-scaffold/lib/Grrr/Taxonomies/ExampleType.php
+++ web/app/themes/wordpress-scaffold/lib/Grrr/Taxonomies/ExampleType.php
@@ -2,14 +2,14 @@
 
 class ExampleType extends TaxonomyAbstract {
 
-    protected $_taxonomy = 'example_type';
-    protected $_slug = 'type';
-    protected $_name = 'Example Types';
-    protected $_singular_name = 'Example Type';
+    protected $_taxonomy           = 'example_type';
+    protected $_slug               = 'type';
+    protected $_name               = 'Example Types';
+    protected $_singular_name      = 'Example Type';
 
     protected $_args = [
         'rewrite' => [
-            'slug' => 'post_type_slug/type',
+            'slug' => '<post_type_slug>/type',
         ],
     ];
 
---
+++ web/app/themes/wordpress-scaffold/template-example.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Example
+ */
+
+$context = Timber::get_context();
+$context['post'] = new Timber\Post();
+$context['templates'] = 'templates/example-template.twig';
+
+Timber::render('base.twig', $context, TWIG_CACHE_TTL);
