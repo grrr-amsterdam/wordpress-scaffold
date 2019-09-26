a62c1c755c31cdcd953d037c2febf7a4361a8e31
Koen Schaft <koen@grrr.nl>
2019-09-26 16:46:26 +0200

    Update scrollListener methods

--- web/app/themes/wordpress-scaffold/assets/scripts/modules/scroll-listener.js
+++ web/app/themes/wordpress-scaffold/assets/scripts/modules/scroll-listener.js
@@ -10,15 +10,13 @@ const onScroll = e => {
   });
 };
 
-export const enhancer = () => {
-  window.addEventListener('scroll', onScroll);
-};
-
 export const register = (key, fn) => {
   listeners[key] = fn;
 };
 
-// Can be called with element or with number
+export const unregister = key => delete listeners[key];
+
+// Can be called with element or with number.
 export const isScrolledPast = elm => {
   if (typeof elm === 'number') {
     return getScrollPosition().y >= elm;
@@ -29,4 +27,19 @@ export const isScrolledPast = elm => {
 
 export const isVisible = (elm, offset = 0) => {
   return elm.getBoundingClientRect().top - window.innerHeight <= offset;
+};
+
+export const isVisibleOrScrolledPast = (elm, offset = 0) => {
+  const dims = elm.getBoundingClientRect();
+  return dims.top - window.innerHeight <= offset;
+};
+
+export const isInView = (elm, offset = 0) => {
+  const top = elm.getBoundingClientRect().top - window.innerHeight <= 0;
+  const bottom = elm.getBoundingClientRect().bottom >= 0;
+  return top && bottom;
+};
+
+export const enhancer = () => {
+  window.addEventListener('scroll', onScroll);
 };
