2d044b4d884c51862d8ced8a972ee6058f1e2cd7
Koen Schaft <koen@grrr.nl>
2019-12-17 11:33:43 +0100

    Simplify GTM enhancer and handle functions

--- web/app/themes/wordpress-scaffold/assets/scripts/modules/gtm-event.js
+++ web/app/themes/wordpress-scaffold/assets/scripts/modules/gtm-event.js
@@ -1,30 +1,25 @@
-const getAttributes = (el) => {
-  return {
-    type: el.getAttribute('data-event-type'),
-    category: el.getAttribute('data-event-cat'),
-    action: el.getAttribute('data-event-action'),
-    label: el.getAttribute('data-event-label'),
-    value: el.getAttribute('data-event-value'),
-  };
-};
+const DEBUG = false;
 
-const hasProductionDefined = () => {
-  window.dataLayer = window.dataLayer || [];
-  return window.dataLayer.length
-    && window.dataLayer[0].platformEnvironment === 'production';
-};
+const getDataLayer = () => window.dataLayer || [];
 
-export const pushEvent = (data) => {
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push(data);
-  if (!hasProductionDefined()) {
+const getAttributes = el => ({
+  type: el.getAttribute('data-event-type'),
+  category: el.getAttribute('data-event-cat'),
+  action: el.getAttribute('data-event-action'),
+  label: el.getAttribute('data-event-label'),
+  value: el.getAttribute('data-event-value'),
+});
+
+export const pushEvent = data => {
+  getDataLayer().push(data);
+  if (DEBUG) {
     console.log('Tracking event', data);
   }
 };
 
 export const trackEvent = ({ type, category, action, label, value }) => {
   if (!type || !category || !action) {
-    console.error('Missing arguments in trackEvent');
+    console.error('Missing arguments in trackEvent.');
     return;
   }
   const data = {
@@ -37,15 +32,11 @@ export const trackEvent = ({ type, category, action, label, value }) => {
   pushEvent({ event: type, ...data });
 };
 
-export const handler = (el, e) => {
-  trackEvent(getAttributes(el));
-};
+export const handler = (el, e) => trackEvent(getAttributes(el));
 
-export const enhancer = (el) => {
-  if (this.nodeName.toUpperCase() !== 'FORM') {
+export const enhancer = el => {
+  if (el.nodeName.toUpperCase() !== 'FORM') {
     console.log('I don\'t know how to enhance non-forms.');
   }
-  this.addEventListener('submit', (e) => {
-    trackEvent(getAttributes(el));
-  });
+  el.addEventListener('submit', e => trackEvent(getAttributes(el)));
 };
