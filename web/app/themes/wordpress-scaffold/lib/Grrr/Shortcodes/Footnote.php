23398f0b15669de9956f9efb09a59d4c9e63cd3a
Koen Schaft <koen@grrr.nl>
2020-01-23 17:16:02 +0100

    Refactor Shortcode example

--- web/app/themes/wordpress-scaffold/lib/Grrr/Shortcodes/Footnote.php
+++ web/app/themes/wordpress-scaffold/lib/Grrr/Shortcodes/Footnote.php
@@ -4,13 +4,13 @@ use Timber;
 
 class Footnote {
 
-    protected $_type = 'footnote';
+    protected $type = 'footnote';
 
     public function register() {
-        add_shortcode($this->_type, [$this, 'shortcode']);
+        add_shortcode($this->type, [$this, 'render']);
     }
 
-    public function shortcode($attributes, $content) {
+    public function render($attributes, $content) {
         $partial = Timber::fetch('lib/Grrr/Shortcodes/templates/footnote.twig', [
             'id' => $attributes['id'],
         ]);
