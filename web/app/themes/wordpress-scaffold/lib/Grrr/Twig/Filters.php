2b17db395aef52124fe4a812eb8474f4347520ef
Koen Schaft <koen@grrr.nl>
2020-02-05 18:10:14 +0100

    Add attrs Twig filter

--- web/app/themes/wordpress-scaffold/lib/Grrr/Twig/Filters.php
+++ web/app/themes/wordpress-scaffold/lib/Grrr/Twig/Filters.php
@@ -5,7 +5,8 @@ use Garp\Functional as f;
 class Filters {
 
     const FUNCTION_MAPPER = [
-        'dropcap' => 'add_dropcap',
+        'attrs'   => 'attrs',
+        'dropcap' => 'dropcap',
         'slugify' => 'make_slug',
     ];
 
@@ -13,7 +14,7 @@ class Filters {
         add_filter('timber/twig', [$this, 'add_filters']);
     }
 
-    public function add_dropcap(string $html, string $class): string {
+    public function dropcap(string $html, string $class): string {
         return preg_replace(
             "@<p>\s*((?:<[^<>]+>\s*)*)([^<>\s])@",
             "<p>$1<span class=\"{$class}\">$2</span>",
@@ -22,7 +23,18 @@ class Filters {
         );
     }
 
-    public function make_slug(string $string): string {
+    public function attrs(array $attributes): string {
+        return f\reduce_assoc(
+            function ($coll, $value, $key) {
+                $attribute = $key . '="' . htmlspecialchars($value, ENT_QUOTES) . '"';
+                return $coll . ($coll ? ' ' : '') . $attribute;
+            },
+            '',
+            $attributes
+        );
+    }
+
+    public function slugify(string $string): string {
         return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string)));
     }
 
