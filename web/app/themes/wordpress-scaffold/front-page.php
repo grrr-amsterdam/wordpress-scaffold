b167175656b88ef4b7b02eb6950181a9a69fd9a9
Koen Schaft <koen@grrr.nl>
2020-02-10 15:42:38 +0100

    Refine front-page Example post type example

--- web/app/themes/wordpress-scaffold/front-page.php
+++ web/app/themes/wordpress-scaffold/front-page.php
@@ -1,5 +1,7 @@
 <?php
 
+use Grrr\PostTypes\Example;
+
 $templates = [
     'front-page.twig',
 ];
@@ -7,5 +9,6 @@ $templates = [
 $context = Timber::get_context();
 $context['post'] = new Timber\Post();
 $context['templates'] = $templates;
+$context['examples'] = (new Example)->get_posts();
 
 Timber::render('base.twig', $context);
