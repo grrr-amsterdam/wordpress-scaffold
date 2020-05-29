41e64e1b66932a19c88b433cd05b56e31922e1c6
Koen Schaft <koen@grrr.nl>
2020-05-29 12:53:59 +0200

    Upgrade @grrr/gulpfile and dependencies

--- web/app/themes/wordpress-scaffold/assets/scripts/modules/cookie-consent.js
+++ web/app/themes/wordpress-scaffold/assets/scripts/modules/cookie-consent.js
@@ -8,10 +8,19 @@ export default () => {
 
   // Update Tag Manager when `update` event is fired.
   cookieConsent.on('update', cookies => {
+
+    // Example 1: Fire for all accepted cookie types.
     cookies.filter(cookie => cookie.accepted).forEach(cookie => pushEvent({
       event: 'cookieConsent',
       cookieType: cookie.id,
     }));
+
+    // Example 2: Fire only the selected cookie type (e.g. when used with radio buttons).
+    pushEvent({
+      event: 'cookieConsent',
+      cookieConsent: cookies.find(cookie => cookie.accepted).id,
+    });
+
   });
 
   // Make the object globally available.
